bd8ce7e0145c91a65b4e8b780e78d753fc806850
shiwenlei <shiwenlei2012@gmail.com>
2018-04-11 16:12:18 +0800

    fix bug in pic_video

--- tools/pic_video.py
+++ tools/pic_video.py
@@ -2,25 +2,28 @@ import cv2
 import glob
 import os
 
-def imgs2video(img_path, destination):
+def imgs2video(img_path, destination, fps=24):
     imgs = list(glob.iglob(img_path+'/*.png'))
     imgs.sort()
     if not len(imgs):
         return
     h, w, c = cv2.imread(imgs[0]).shape
-    video = cv2.VideoWriter(destination, -1, 1, (w, h))
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    video = cv2.VideoWriter(destination, fourcc, 24, (w, h))
     print('video '+ destination + ' starts...')
     for img in imgs:
         print ('writing '+ img)
         video.write(cv2.imread(img))
     video.release()
 
+
 if __name__ == '__main__':
-    sp = '/home/shiwenlei/experiment/Data/MOT17/train'
+    sp = '/home/shiwenlei/experiment/Data/MOT17/infer/detectron'
     flist = os.listdir(sp)
     for f in flist:
         vname = os.path.join(sp, f+'.avi')
         img_path = os.path.join(sp, f+'/img1')
+        print (img_path)
         imgs2video(img_path, vname)
 
 
