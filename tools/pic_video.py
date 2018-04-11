175b7f470a2b1ee6c2c121bd7db82febfd3f9ac3
AlexKing1028 <shiwenlei2012@gmail.com>
2018-04-11 16:44:52 +0800

    video to pictures

--- tools/pic_video.py
+++ tools/pic_video.py
@@ -1,6 +1,7 @@
 import cv2
 import glob
 import os
+import sys
 
 def imgs2video(img_path, destination, fps=24):
     imgs = list(glob.iglob(img_path+'/*.png'))
@@ -9,15 +10,34 @@ def imgs2video(img_path, destination, fps=24):
         return
     h, w, c = cv2.imread(imgs[0]).shape
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    video = cv2.VideoWriter(destination, fourcc, 24, (w, h))
-    print('video '+ destination + ' starts...')
+    video = cv2.VideoWriter(destination, fourcc, fps, (w, h))
+    # print('video '+ destination + ' starts...')
     for img in imgs:
         print ('writing '+ img)
         video.write(cv2.imread(img))
     video.release()
 
 
+def video2imgs(v_path, destinationm, num=0):
+    v_name = os.path.basename(v_path)
+    video = cv2.VideoCapture(v_path)
+    rval = video.isOpened()
+    if (rval && num <= 0)
+        num = sys.maxints
+    cnt = 0
+    while(rval && num >0)
+        rval, frame = video.read()
+        cnt = cnt+1
+        num = num-1
+        img_name = v_name+'_'+str(cnt).zfill(10)+'.jpg'
+        print('saving '+img_name +'...')
+        cv2.imwrite(os.path.join(destination, img_name), frame)
+    video.release()
+    return cnt
+
+
 if __name__ == '__main__':
+    '''
     sp = '/home/shiwenlei/experiment/Data/MOT17/infer/detectron'
     flist = os.listdir(sp)
     for f in flist:
@@ -25,5 +45,10 @@ if __name__ == '__main__':
         img_path = os.path.join(sp, f+'/img1')
         print (img_path)
         imgs2video(img_path, vname)
+    '''
+    sp = '/home/shiwenlei/Downloads/nihao.mp4'
+    dest = '/home/shiwenlei/experiment/Data/Detectron/nihao'
+    video2imgs(sp, destination, num=7200)
+
 
 
